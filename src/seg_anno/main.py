1bc26a0963f8751420261c66a11214b01a524e5a
WenJiarui <wenjr@cetcmotor.com>
2017-06-20 14:22:50 +0800

    加入原始seg文件的载入，加入默认类别设置，增益保存设置

--- src/seg_anno/main.py
+++ src/seg_anno/main.py
@@ -18,14 +18,16 @@ for key,item in items_table.items():
     if key.find("class_")==-1:
         continue
     try:
-        id,color = item.split(' ')
-        id = int(id)
+        _id,color = item.split(' ')
+        _id = int(_id)
         color = color[1:-1]
         color = tuple([int(c) for c in color.split(',')])
-        label_table[id] = {"label":key, "color":color}
+        label_table[_id] = {"label":key, "color":color}
     except:
         pass
 img_path = items_table["img_path"]
+label_path = img_path.split(".")[0]+"_seq.png" 
+
 img_load = cv2.imread(img_path)
 print("please input '1' to '9'")
 h,w,_ = img_load.shape
@@ -33,7 +35,21 @@ h,w,_ = img_load.shape
 pad = items_table["pad"]
 linewidth = items_table["linewidth"]
 img_draw = np.zeros((h+pad*2,w+pad*2,3),np.uint8)
-img_label = np.zeros((h+pad*2,w+pad*2),np.uint8) + 2
+
+if "def_class" in items_table:
+    key = items_table["def_class"]
+    _id,_ =  items_table[key].split(' ')
+    _id = int(_id)
+    img_label = np.zeros((h+pad*2,w+pad*2),np.uint8) + _id
+    
+else:
+    img_label = np.zeros((h+pad*2,w+pad*2),np.uint8)
+    
+if os.path.exists(label_path):
+    img_label_r = cv2.imread(label_path)[:,:,0] / items_table["color_gain_save"]
+    img_label[pad:pad+h,pad:pad+w] = img_label_r[:]
+
+
 img_draw_stack = []
 img_label_stack = []
 _img_draw = np.zeros((h+pad*2,w+pad*2,3),np.uint8)
@@ -61,7 +77,7 @@ def draw_foldline(img, pts):
         cv2.line(img, (x0,y0),(x1,y1),cur_line_color,linewidth)
 
 def disp_gray(img2d):
-    imgshow = img2d * 25
+    imgshow = img2d * items_table["color_gain_disp"]
     return imgshow
 
 def fill_ploygon(img, pts):
@@ -102,12 +118,16 @@ while(1):
     img_roi[img_roi==0] = img_load[img_roi==0]
     cv2.imshow('image',img_draw)
     cv2.imshow('label',disp_gray(img_label))
+    cv2.moveWindow('image',0,0)
+    cv2.moveWindow('label',w+pad+40,0)
 
     k=cv2.waitKey(1)&0xFF
     if 49 <= k <= 57:
         cur_label = k - 48
-        cur_line_color = label_table[cur_label]['color']
-        print("curlabel:",cur_label,label_table[cur_label]['label'])
+        if cur_label in label_table:
+            cur_line_color = label_table[cur_label]['color']
+        else:
+            print("invalid num input")
     elif k == 48:
         cur_line_color = label_table[cur_label]['color']
         
@@ -133,9 +153,9 @@ while(1):
             img_label[:] = img_label_stack[-1]
         
     elif k==13: # 按Enter退出 并 保存
-        img_sav = img_label[pad:pad+w,pad:pad+h]
-        cv2.imwrite(img_path.split(".")[0]+"_seq.png",img_sav)
-        print("save", img_path.split(".")[0] + "_seq.png")
+        img_sav = img_label[pad:pad+h,pad:pad+w] * items_table["color_gain_save"]
+        cv2.imwrite(label_path,img_sav)
+        print("save", label_path)
         break
     
     elif k==27: # 按Esc退出
